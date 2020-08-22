03fe3fd6b34678b80718be410acbc980f3a52ba1
SevenR23 <sevenr23@gmail.com>
2020-08-22 22:50:49 +0300

    Moved the scripts to a separate file.

---
+++ Content/SjopJS/IndexViewJS.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+
+    /* Confirm record deletion */
+    $("a.delete").click(function () {
+        if (!confirm("Confirm page deletion")) return false;
+    });
+
+    /* Sorting script */
+    $("table#pages tbody").sortable({
+        items: "tr:not(.headline)",
+        placeholder: "ui-state-highlight",
+        update: function () {
+            var ids = $("table#pages tbody").sortable("serialize");
+            var url = "/Admin/Pages/ReorderRecords";
+
+            $.post(url, ids, function (data) {
+
+            });
+        }
+    });
+});
