6b2f60e72ef158cf8a49143fedbc6b4f0014ba94
SevenR23 <sevenr23@gmail.com>
2020-08-22 09:04:51 +0300

    Added option to add category.

--- Areas/Admin/Controllers/ShopController.cs
+++ Areas/Admin/Controllers/ShopController.cs
@@ -21,9 +21,68 @@ namespace Store_MVC.Areas.Admin.Controllers
                 // Инициализируем модель данными
                 categoryVMList = db.Categories.ToArray().OrderBy(x => x.Sorting).Select(x => new CategoryVM(x)).ToList();
             }
-            // Возвращаем List в представление
 
+            // Возвращаем List в представление
             return View(categoryVMList);
+        }
+
+        // POST: Admin/Shop/AddNewCategory
+        [HttpPost]
+        public string AddNewCategory(string categoryName)
+        {
+            // Строковая переменная ID 
+            string id;
+
+            using (Db db = new Db())
+            {
+                // Проверить на уникальность
+                if (db.Categories.Any(x => x.Name == categoryName)) { return "titletaken"; }
+
+                // Иницализировать модель DTO
+                CategoryDTO dto = new CategoryDTO();
+
+                // Заполняем данными модель
+                dto.Name = categoryName;
+                dto.ShortDesc = categoryName.Replace(" ", "-").ToLower();
+                dto.Sorting = 100;
+
+                // Сохранить
+                db.Categories.Add(dto);
+                db.SaveChanges();
+
+                // Получаем ID для возврата в представление 
+                id = dto.Id.ToString();
+            }
+
+            // Вернуть ID в представление
+            return id;
+        }
+
+        // Метод сортировки
+        // POST: Admin/Shop/ReorderCategories
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+
+            using (Db db = new Db())
+            {
+                // Реализуем начальный счётчик
+                int count = 0;
+
+                // Инициализируем модель данных
+                CategoryDTO dto;
+
+                // Устанавливаем сортирвку для каждой страницы
+                foreach (var categoryId in id)
+                {
+                    dto = db.Categories.Find(categoryId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+
+                    count++;
+                }
+            }
         }
     }
 }
