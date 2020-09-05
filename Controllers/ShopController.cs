3e71c239c59bda48719d116bea3617e53d78ecd3
SevenR23 <sevenr23@gmail.com>
2020-09-05 13:21:42 +0300

    Added a list of products by category.

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -30,5 +30,41 @@ namespace Store_MVC.Controllers
             // Возвращаем частичное представление с моделью
             return PartialView("_CategoryMenuPartial", categoryVMList);
         }
+
+        // Метод выводит список товаров по категориям
+        // GET: Shop/Category/name
+        public ActionResult Category(string name)
+        {
+            // Объявляем список типа List
+            List<ProductVM> productVMList;
+
+            using (Db db = new Db())
+            {
+                // Получаем id категории
+                CategoryDTO dto = db.Categories.Where(x => x.ShortDesc == name).FirstOrDefault();
+
+                int catId = dto.Id;
+
+                // Инициализируем наш список данными
+                productVMList = db.Products.ToArray().Where(x => x.CategoryId == catId).Select(x => new ProductVM(x)).ToList();
+
+                //Получаем имя категории
+                var productCategory = db.Products.Where(x => x.CategoryId == catId).FirstOrDefault();
+
+                // Проверяем на null
+                if (productCategory == null)
+                {
+                    var catName = db.Categories.Where(x => x.ShortDesc == name).Select(x => x.Name).FirstOrDefault();
+                    ViewBag.CategoryName = catName;
+                }
+                else
+                {
+                    ViewBag.CategoryName = productCategory.CategoryName;
+                }
+            }
+
+            //Возвращаем представление с моделью
+            return View(productVMList);
+        }
     }
 }
