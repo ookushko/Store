4439683d297c1b93bac15eecb532f5470b9cca8b
AlexandrOkushko <sevenr23@gmail.com>
2020-09-06 12:24:55 +0300

    Added 'Cartpartial' basket method and view model.

---
+++ Controllers/CartController.cs
@@ -0,0 +1,52 @@
+﻿using Store_MVC.Models.ViewModels.Cart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Store_MVC.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CartPartial()
+        {
+            // Объявляем модель CartVM
+            CartVM model = new CartVM();
+
+            // Объявляем переменную количества
+            int quantety = 0;
+
+            // Объявляем переменную цены
+            decimal price = 0;
+
+            // Проверяем сессию корзины
+            if (Session["cart"] != null)
+            {
+                // Получаем общее количество товаров и цену
+                var list = (List<CartVM>)Session["cart"];
+
+                foreach (var item in list)
+                {
+                    quantety += item.Quantity;
+                    price += item.Quantity * item.Price;
+                }
+            }
+            else
+            {
+                // Или устанавливаем количество и цену = 0
+                model.Quantity = 0;
+                model.Price = 0m;
+            }
+
+            // Возвращаем частичное представление с моделью
+            return PartialView("_CartPartial", model);
+        }
+    }
+}
---
+++ Models/ViewModels/Cart/CartVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Store_MVC.Models.ViewModels.Cart
+{
+    public class CartVM
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Total 
+        {
+            get { return Quantity * Price; } 
+        }
+        public string Image { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
