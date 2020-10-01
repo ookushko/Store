fe8d1a8f5e81dce0bc531aedf184afa84420a5b3
AlexandrOkushko <sevenr23@gmail.com>
2020-10-01 22:11:56 +0300

    Add [HttpPost] for 'Login' method, now the data will be stored in the database at registration.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Store_MVC.Models.Data;
+using Store_MVC.Models.ViewModels.Account;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -20,6 +22,70 @@ namespace Store_MVC.Controllers
         public ActionResult CreateAccount()
         {
             return View("CreateAccount");
+        }
+
+        // POST: account/create-account
+        [ActionName("create-account")]
+        [HttpPost]
+        public ActionResult CreateAccount(UserVM model)
+        {
+            // Проверяем модель на валидность
+            if (!ModelState.IsValid)
+            {
+                return View("CreateAccount", model);
+            }
+
+            // Проверяем соответствие пароля
+            if (!model.Password.Equals(model.ConfirmPassword))
+            {
+                ModelState.AddModelError("", "Password do not match.");
+                return View("CreateAccount", model);
+            }
+
+            using (Db db = new Db())
+            {
+                // Проверяем уникально ли имя
+                if (db.Users.Any(x => x.Username.Equals(model.Username)))
+                {
+                    ModelState.AddModelError("", $"Username {model.Username} is taken.");
+                    model.Username = "";
+                    return View("CreateAccount", model);
+                }
+
+                // Создаём экземпляр класса UserDTO
+                UserDTO userDTO = new UserDTO()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    EmailAdress = model.EmailAdress,
+                    Username = model.Username,
+                    Password = model.Password
+                };
+
+                // Добавляем данные в модель
+                db.Users.Add(userDTO);
+
+                // Сохранить данные
+                db.SaveChanges();
+
+                // Добавляем роль пользователю
+                int id = userDTO.Id;
+
+                UserRoleDTO userRoleDTO = new UserRoleDTO()
+                {
+                    UserId = id,
+                    RoleId = 2 // Роль обычного юзера
+                };
+
+                db.UserRoles.Add(userRoleDTO);
+                db.SaveChanges();
+            }
+
+            // Записываем сообщение в TempData
+            TempData["SM"] = "You are now registered and can login.";
+
+            // Переадресовываем пользователя
+            return RedirectToAction("Login");
         }
 
         // GET: Account/Login
