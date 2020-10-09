03833536d24b7c624bddeb3db71831b380963b38
AlexandrOkushko <sevenr23@gmail.com>
2020-10-09 22:41:35 +0300

    Add 'UserProfile' method and view.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -162,5 +162,83 @@ namespace Store_MVC.Controllers
             return PartialView(model);
         }
 
+        // GET: /account/user-profile
+        [HttpGet]
+        [ActionName("user-profile")]
+        public ActionResult UserProfile()
+        {
+            // Получаем имя пользователя
+            string userName = User.Identity.Name;
+
+            // Объявляем модель
+            UserProfileVM model;
+
+            using (Db db = new Db())
+            {
+                // Получаем пользователя
+                UserDTO dto = db.Users.FirstOrDefault(x => x.Username == userName);
+
+                // Инициализируем модель данными
+                model = new UserProfileVM(dto);
+            }
+
+            // Возвращаем модель в представление
+            return View("UserProfile", model);
+        }
+
+        // POST: /account/user-profile
+        [HttpPost]
+        [ActionName("user-profile")]
+        public ActionResult UserProfile(UserProfileVM model) 
+        {
+            // Проверяем модель на валидность
+            if (!ModelState.IsValid)
+            {
+                return View("UserProfile", model);
+            }
+
+            // Проверяем пароль (если пользователь его меняет)
+            if (!string.IsNullOrWhiteSpace(model.Password) && !model.Password.Equals(model.ConfirmPassword))
+            {
+                ModelState.AddModelError("", "Password do not match.");
+                return View("UserProfile", model);
+            }
+
+            using (Db db = new Db())
+            {
+                // Получаем имя пользователя
+                string userName = User.Identity.Name;
+
+                // Проверяем имя на уникальность
+                if (db.Users.Where(x => x.Id != model.Id).Any(x => x.Username == userName))
+                {
+                    ModelState.AddModelError("", $"Username {model.Username} already exists.");
+                    model.Username = "";
+                    return View("UserProfile", model);
+                }
+
+                // Изменяем модель контекста данных
+                UserDTO dto = db.Users.Find(model.Id);
+
+                dto.FirstName = model.FirstName;
+                dto.LastName = model.LastName;
+                dto.EmailAdress = model.EmailAdress;
+                dto.Username = model.Username;
+
+                if (!string.IsNullOrWhiteSpace(model.Password))
+                {
+                    dto.Password = model.Password;
+                }
+
+                // Сохраняем изменения
+                db.SaveChanges();
+            }
+
+            // Устанавливаем сообщение в TempData
+            TempData["SM"] = "You have edited your profile.";
+
+            // Возвращаем представление с моделью
+            return View("UserProfile", model);
+        }
     }
 }
---
+++ Models/ViewModels/Account/UserProfileVM.cs
@@ -0,0 +1,46 @@
+﻿using Store_MVC.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Store_MVC.Models.ViewModels.Account
+{
+    public class UserProfileVM
+    {
+        public UserProfileVM()
+        {
+
+        }
+
+        public UserProfileVM(UserDTO row)
+        {
+            Id = row.Id;
+            FirstName = row.FirstName;
+            LastName = row.LastName;
+            EmailAdress = row.EmailAdress;
+            Username = row.Username;
+            Password = row.Password;
+        }
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string EmailAdress { get; set; }
+        [Required]
+        [Display(Name = "User name")]
+        public string Username { get; set; }
+        [DataType(DataType.Password)]
+        [StringLength(50, MinimumLength = 6)]
+        public string Password { get; set; }
+        [Display(Name = "Confirm password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
