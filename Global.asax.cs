74eca447e2db499eeb9483b2651eb961fdf68a83
AlexandrOkushko <sevenr23@gmail.com>
2020-10-13 21:10:56 +0300

    Add method of processing of requests for auttefication.

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Store_MVC.Models.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +18,39 @@ namespace Store_MVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        /// <summary>
+        /// Метод обработки запросов аунтефикации
+        /// </summary>
+        protected void Application_AutentificateRequest()
+        {
+            // Проверяем авторизацию пользователя
+            if (User == null) return;
+
+            // Получаем имя пользователя
+            string userName = Context.User.Identity.Name;
+
+            // Объявляем массив ролей
+            string[] roles = null;
+
+            using (Db db = new Db())
+            {
+                // Заполняем роли
+                UserDTO dto = db.Users.FirstOrDefault(x => x.Username == userName);
+
+                // Выполняем проверку на null, который возникает при смене Username
+                if (dto == null) return;
+
+                roles = db.UserRoles.Where(x => x.UserId == dto.Id).Select(x => x.Role.Name).ToArray();
+            }
+
+            // Создаем объект интерфейс IPrincipal 
+            IIdentity userIdentity = new GenericIdentity(userName);
+            IPrincipal newUserObj = new GenericPrincipal(userIdentity, roles);
+
+            // Объявляем и инициализируем Context.User данными
+            Context.User = newUserObj;
         }
     }
 }
