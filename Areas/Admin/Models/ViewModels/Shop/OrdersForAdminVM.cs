efb08fd98114c654ea3b4a5c3ea5c4f4587d1a9c
AlexandrOkushko <sevenr23@gmail.com>
2020-10-20 19:19:11 +0300

    Add 'OrdersForAdminVM' view model class.

---
+++ Areas/Admin/Models/ViewModels/Shop/OrdersForAdminVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Store_MVC.Areas.Admin.Models.ViewModels.Shop
+{
+    public class OrdersForAdminVM
+    {
+        public int OrderNumber { get; set; }
+        public string UserName { get; set; }
+        public decimal Total { get; set; }
+        public Dictionary<string, int> ProductsAndQuantity { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
